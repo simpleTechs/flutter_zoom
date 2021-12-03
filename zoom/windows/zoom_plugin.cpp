8b3c4b783fb3565bb87651044d68d7d0f7678037
Mathis Neumann <gitlab@madd.is>
2021-12-03 17:38:02 +0100

    support event streams for meeting status

--- zoom/windows/zoom_plugin.cpp
+++ zoom/windows/zoom_plugin.cpp
@@ -9,12 +9,16 @@
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar_windows.h>
 #include <flutter/standard_method_codec.h>
+#include <flutter/event_stream_handler.h>
+#include <flutter/event_channel.h>
 
-#include "./zoom_sdk/h/zoom_sdk.h"
-#include "./zoom_sdk/h/zoom_sdk_ext.h"
-#include "./zoom_sdk/h/meeting_service_interface.h"
-// #include "./zoom_sdk/h/meeting_service_components/meeting_ui_ctrl_interface.h"
-#include "./zoom_sdk/h/auth_service_interface.h"
+#include "zoom_sdk/h/zoom_sdk.h"
+#include "zoom_sdk/h/zoom_sdk_ext.h"
+#include "zoom_sdk/h/meeting_service_interface.h"
+#include "zoom_sdk/h/meeting_service_components/meeting_ui_ctrl_interface.h"
+#include "zoom_sdk/h/meeting_service_components/meeting_configuration_interface.h"
+#include "zoom_sdk/h/meeting_service_components/meeting_sharing_interface.h"
+#include "zoom_sdk/h/auth_service_interface.h"
 
 #include <map>
 #include <memory>
@@ -25,40 +29,190 @@
 namespace
 {
 
+  std::string mapStatusToName(ZOOM_SDK_NAMESPACE::MeetingStatus status);
+
+  template <typename T = flutter::EncodableValue>
+  class ZoomPluginStreamHandler : public flutter::StreamHandler<T>, public ZOOM_SDK_NAMESPACE::IMeetingServiceEvent
+  {
+  public:
+    std::unique_ptr<flutter::EventSink<flutter::EncodableValue>> eventSink;
+    ZOOM_SDK_NAMESPACE::IMeetingService *meetingService;
+
+    ZoomPluginStreamHandler() = default;
+    virtual ~ZoomPluginStreamHandler() = default;
+
+    // included from IMeetingServiceEvent:
+    /// \brief Meeting status changed callback.
+    /// \param status The value of meeting. For more details, see \link MeetingStatus \endlink.
+    /// \param iResult Detailed reasons for special meeting status.
+    ///If the status is MEETING_STATUS_FAILED, the value of iResult is one of those listed in MeetingFailCode enum.
+    ///If the status is MEETING_STATUS_ENDED, the value of iResult is one of those listed in MeetingEndReason.
+    void onMeetingStatusChanged(ZOOM_SDK_NAMESPACE::MeetingStatus status, int iResult = 0) override;
+
+    /// \brief Meeting statistics warning notification callback.
+    /// \param type The warning type of the meeting statistics. For more details, see \link StatisticsWarningType \endlink.
+    void onMeetingStatisticsWarningNotification(ZOOM_SDK_NAMESPACE::StatisticsWarningType type) override;
+
+    /// \brief Meeting parameter notification callback.
+    /// \param meeting_param Meeting parameter. For more details, see \link MeetingParameter \endlink.
+    /// \remarks The callback will be triggered right before the meeting starts. The meeting_param will be destroyed once the function calls end.
+    void onMeetingParameterNotification(const ZOOM_SDK_NAMESPACE::MeetingParameter *meeting_param) override;
+
+  protected:
+    // ~ZoomPluginStreamHandler();
+
+    // ZoomPluginStreamHandler(flutter::EventChannel<T> *channel,
+    //                         ZOOM_SDK_NAMESPACE::IMeetingService *meetingService);
+
+    // from flutter StreamHandler
+    // Implementation of the public interface, to be provided by subclasses.
+    std::unique_ptr<flutter::StreamHandlerError<T>> OnListenInternal(
+        const T *arguments,
+        std::unique_ptr<flutter::EventSink<T>> &&events);
+
+    // Implementation of the public interface, to be provided by subclasses.
+    std::unique_ptr<flutter::StreamHandlerError<T>> OnCancelInternal(
+        const T *arguments) override;
+  };
+
+  // {
+  // }
+  // ZoomPluginStreamHandler::ZoomPluginStreamHandler(flutter::EventChannel<flutter::EncodableValue> *channel,
+  //                                                  ZOOM_SDK_NAMESPACE::IMeetingService *meetingService)
+  // {
+  //     this->channel = channel;
+  //     this->meetingService = meetingService;
+  // }
+
+  std::unique_ptr<flutter::StreamHandlerError<flutter::EncodableValue>> ZoomPluginStreamHandler<flutter::EncodableValue>::OnListenInternal(
+      const flutter::EncodableValue *arguments,
+      std::unique_ptr<flutter::EventSink<flutter::EncodableValue>> &&events)
+  {
+    this->eventSink = std::move(events);
+    // TODO:
+    return nullptr;
+  };
+
+  // Implementation of the public interface, to be provided by subclasses.
+  std::unique_ptr<flutter::StreamHandlerError<flutter::EncodableValue>> ZoomPluginStreamHandler<flutter::EncodableValue>::OnCancelInternal(
+      const flutter::EncodableValue *arguments)
+  {
+    this->eventSink = NULL;
+    // TODO:
+    return nullptr;
+  };
+
+  void ZoomPluginStreamHandler<flutter::EncodableValue>::onMeetingStatusChanged(ZOOM_SDK_NAMESPACE::MeetingStatus status, int iResult)
+  {
+    auto statusName = mapStatusToName(status);
+    if (this->eventSink)
+    {
+      auto out = flutter::EncodableList();
+      out.push_back(flutter::EncodableValue(statusName.c_str()));
+      out.push_back(flutter::EncodableValue(""));
+      this->eventSink->Success(out);
+    }
+    // TODO
+  }
+
+  void ZoomPluginStreamHandler<flutter::EncodableValue>::onMeetingStatisticsWarningNotification(ZOOM_SDK_NAMESPACE::StatisticsWarningType type)
+  {
+    // TODO
+  }
+  void ZoomPluginStreamHandler<flutter::EncodableValue>::onMeetingParameterNotification(const ZOOM_SDK_NAMESPACE::MeetingParameter *meeting_param)
+  {
+    // TODO
+  }
+
+  std::string mapStatusToName(ZOOM_SDK_NAMESPACE::MeetingStatus status)
+  {
+    switch (status)
+    {
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_IDLE:
+      return "MEETING_STATUS_IDLE";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_CONNECTING:
+      return "MEETING_STATUS_CONNECTING";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_WAITINGFORHOST:
+      return "MEETING_STATUS_WAITINGFORHOST";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_INMEETING:
+      return "MEETING_STATUS_INMEETING";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_DISCONNECTING:
+      return "MEETING_STATUS_DISCONNECTING";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_RECONNECTING:
+      return "MEETING_STATUS_RECONNECTING";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_FAILED:
+      return "MEETING_STATUS_FAILED";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_ENDED:
+      return "MEETING_STATUS_ENDED";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_UNKNOW:
+      return "MEETING_STATUS_UNKNOW";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_LOCKED:
+      return "MEETING_STATUS_LOCKED";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_UNLOCKED:
+      return "MEETING_STATUS_UNLOCKED";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_IN_WAITING_ROOM:
+      return "MEETING_STATUS_IN_WAITING_ROOM";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_WEBINAR_PROMOTE:
+      return "MEETING_STATUS_WEBINAR_PROMOTE";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_WEBINAR_DEPROMOTE:
+      return "MEETING_STATUS_WEBINAR_DEPROMOTE";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_JOIN_BREAKOUT_ROOM:
+      return "MEETING_STATUS_JOIN_BREAKOUT_ROOM";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_LEAVE_BREAKOUT_ROOM:
+      return "MEETING_STATUS_LEAVE_BREAKOUT_ROOM";
+    case ZOOM_SDK_NAMESPACE::MEETING_STATUS_WAITING_EXTERNAL_SESSION_KEY:
+      return "MEETING_STATUS_WAITING_EXTERNAL_SESSION_KEY";
+    default:
+      return "UNKNOWN";
+    }
+  }
+
+  // main plugin for flutter
   class ZoomPlugin : public flutter::Plugin
   {
   public:
+    // static ZOOM_SDK_NAMESPACE::IMeetingService *meetingService;
     static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
-    ZOOM_SDK_NAMESPACE::IMeetingService *_meetingService = nullptr;
-    ZOOM_SDK_NAMESPACE::IMeetingService *getMeetingService();
+
+    static ZOOM_SDK_NAMESPACE::IMeetingService *_meetingService;
+    static ZOOM_SDK_NAMESPACE::IMeetingService *getMeetingService();
 
     ZoomPlugin();
 
     virtual ~ZoomPlugin();
     void ZoomPlugin::joinMeeting(const flutter::MethodCall<flutter::EncodableValue> &method_call,
                                  std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+    void ZoomPlugin::getMeetingStatus(std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
 
   private:
+    static ZoomPluginStreamHandler<flutter::EncodableValue> *streamHandler;
     // Called when a method is called on this plugin's channel from Dart.
     void HandleMethodCall(
         const flutter::MethodCall<flutter::EncodableValue> &method_call,
         std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+
     void AuthorizeSDK(const std::wstring domain, const std::wstring jwt_token, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+
     void init(const flutter::MethodCall<flutter::EncodableValue> &method_call,
               std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
 
+    /// listen to zoom sdk messages when initializing the plugin
     class ZoomPluginAuthListener : public ZOOM_SDK_NAMESPACE::IAuthServiceEvent
     {
     public:
       std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> init_result;
-      ZoomPluginAuthListener(std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> res)
+      ZoomPlugin *plugin;
+      ZoomPluginAuthListener(std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> res, ZoomPlugin *plugin)
       {
         this->init_result = std::move(res);
+        this->plugin = plugin;
       }
       void onAuthenticationReturn(ZOOM_SDK_NAMESPACE::AuthResult result)
       {
         // flutter::EncodableValue res = flutter::EncodableValue(result); dart expects List<dynamic>
         this->init_result->Success();
+        auto service = getMeetingService();
+        service->SetEvent(streamHandler); // TODO: pass reference to stream handler
       }
 
       void onLoginReturnWithReason(ZOOM_SDK_NAMESPACE::LOGINSTATUS ret, ZOOM_SDK_NAMESPACE::IAccountInfo *pAccountInfo, ZOOM_SDK_NAMESPACE::LoginFailReason reason)
@@ -80,7 +234,12 @@ namespace
         //
       }
     };
+
+  private:
   };
+
+  ZoomPluginStreamHandler<flutter::EncodableValue> *ZoomPlugin::streamHandler = NULL;
+  ZOOM_SDK_NAMESPACE::IMeetingService *ZoomPlugin::_meetingService = NULL;
 
   // static
   void ZoomPlugin::RegisterWithRegistrar(
@@ -99,6 +258,21 @@ namespace
           plugin_pointer->HandleMethodCall(call, std::move(result));
         });
 
+    auto eventChannel =
+        std::make_unique<flutter::EventChannel<flutter::EncodableValue>>(
+            registrar->messenger(), "plugins.webcare/zoom_event_stream",
+            &flutter::StandardMethodCodec::GetInstance());
+
+    // TODO: how do I pass both the meeting service and event channel to the stream handler
+    // idea: only call set stream handler once zoom is ready
+    // however, how can I convert this pointer to make it work?!
+    // adapted from https://gitea.com/fawdlstty/test_camera/src/branch/master/windows/test_camera_plugin.cpp
+    streamHandler = new ZoomPluginStreamHandler<flutter::EncodableValue>();
+    flutter::StreamHandler<flutter::EncodableValue> *handler = static_cast<flutter::StreamHandler<flutter::EncodableValue> *>(streamHandler);
+    //std::unique_ptr<flutter::StreamHandler<flutter::EncodableValue>> handler = std::make_unique<flutter::StreamHandler<flutter::EncodableValue>>(plugin->eventChannel);
+    std::unique_ptr<flutter::StreamHandler<flutter::EncodableValue>> _ptr{handler};
+    eventChannel->SetStreamHandler(std::move(_ptr));
+
     registrar->AddPlugin(std::move(plugin));
   }
 
@@ -108,18 +282,19 @@ namespace
 
   ZOOM_SDK_NAMESPACE::IMeetingService *ZoomPlugin::getMeetingService()
   {
-    if (!this->_meetingService)
+    if (!_meetingService)
     {
 
-      ZOOM_SDK_NAMESPACE::IMeetingService *meetingService;
-      ZOOM_SDK_NAMESPACE::SDKError meetingServiceInitReturnVal = ZOOM_SDK_NAMESPACE::CreateMeetingService(&meetingService);
+      ZOOM_SDK_NAMESPACE::IMeetingService *service;
+      ZOOM_SDK_NAMESPACE::SDKError meetingServiceInitReturnVal = ZOOM_SDK_NAMESPACE::CreateMeetingService(&service);
       if (meetingServiceInitReturnVal == ZOOM_SDK_NAMESPACE::SDKError::SDKERR_SUCCESS)
       {
         // MeetingService was created successfully, it is recommended to store this value for use across the application
-        this->_meetingService = meetingService;
+        _meetingService = service;
+        ZoomPlugin::_meetingService = service;
       }
     }
-    return this->_meetingService;
+    return _meetingService;
   }
 
   std::wstring toString(std::string str)
@@ -166,7 +341,8 @@ namespace
 
         // Call SetEvent to assign your IAuthServiceEvent listener
         // TODO: handle errors
-        ZoomPluginAuthListener *yourAuthServiceEventListener = new ZoomPluginAuthListener(std::move(result));
+        ZoomPluginAuthListener *yourAuthServiceEventListener = new ZoomPluginAuthListener(std::move(result), this);
+
         authListener = yourAuthServiceEventListener;
         authService->SetEvent(authListener);
 
@@ -198,7 +374,7 @@ namespace
         return toString(std::get<std::string>(result_it->second));
       }
     }
-    return nullptr;
+    return NULL;
   }
   void ZoomPlugin::joinMeeting(const flutter::MethodCall<flutter::EncodableValue> &method_call,
                                std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
@@ -235,7 +411,7 @@ namespace
       joinParams.customer_key = NULL;
       joinParams.webinarToken = NULL;
       joinParams.isVideoOff = false;
-      joinParams.isAudioOff = false;
+      joinParams.isAudioOff = noAudio == L"true";
       joinParams.isDirectShareDesktop = false;
 
       joinMeetingParam.userType = ZOOM_SDK_NAMESPACE::SDK_UT_NORMALUSER;
@@ -245,14 +421,19 @@ namespace
       auto meetingService = this->getMeetingService();
 
       // TODO: support more sharing options
-      // meetingService->GetUIController()->ShowSharingToolbar(!disableShare);
+      auto uiController = meetingService->GetUIController();
+
+      if (uiController)
+      {
+        uiController->ShowSharingToolbar(disableShare == L"true");
+      }
 
       joinMeetingCallReturnValue = meetingService->Join(joinMeetingParam);
       if (joinMeetingCallReturnValue == ZOOM_SDK_NAMESPACE::SDKError::SDKERR_SUCCESS)
       {
         // Join meeting call succeeded, listen for join meeting result using the onMeetingStatusChanged callback
         flutter::EncodableValue res = flutter::EncodableValue(joinMeetingCallReturnValue);
-        result->Success(res);
+        result->Success(true);
       }
       else
       {
@@ -292,6 +473,39 @@ namespace
     }
   }
 
+  void ZoomPlugin::getMeetingStatus(std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
+  {
+    auto meetingService = this->getMeetingService();
+    if (meetingService)
+    {
+      auto status = meetingService->GetMeetingStatus();
+      if (status == NULL)
+      {
+        auto out = flutter::EncodableList();
+        out.push_back(flutter::EncodableValue("MEETING_STATUS_UNKNOWN"));
+        out.push_back(flutter::EncodableValue("No status available"));
+        result->Success(out);
+      }
+      else
+      {
+        auto statusName = mapStatusToName(status);
+        // we just try to do the same as android, this structure is slightly weird
+        auto out = flutter::EncodableList();
+
+        out.push_back(flutter::EncodableValue(statusName));
+        out.push_back(flutter::EncodableValue(""));
+        result->Success(out);
+      }
+    }
+    else
+    {
+      auto out = flutter::EncodableList();
+      out.push_back(flutter::EncodableValue("MEETING_STATUS_UNKNOWN"));
+      out.push_back(flutter::EncodableValue("No status available"));
+      result->Success(out);
+    }
+  }
+
   void ZoomPlugin::HandleMethodCall(
       const flutter::MethodCall<flutter::EncodableValue> &method_call,
       std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
@@ -304,6 +518,10 @@ namespace
     else if (method_call.method_name().compare("init") == 0)
     {
       this->init(method_call, std::move(result));
+    }
+    else if (method_call.method_name().compare("meeting_status") == 0)
+    {
+      this->getMeetingStatus(std::move(result));
     }
     else if (method_call.method_name().compare("getPlatformVersion") == 0)
     {
@@ -329,7 +547,6 @@ namespace
       result->NotImplemented();
     }
   }
-
 } // namespace
 
 void ZoomPluginRegisterWithRegistrar(
